47bcc28b72e0231d2e3519fbbf8cfee0511f9972
salgadotomas89 <salgadotomas@outlook.com>
2024-08-23 09:58:26 -0400

    modificando build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+echo "BUILD START"
+python -m ensurepip --upgrade
+pip install --upgrade pip
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+echo "BUILD END"
