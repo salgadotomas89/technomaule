8d77a4ef866e21bcaee70e7b68b2da546230d439
salgadotomas89 <salgadotomas@outlook.com>
2024-08-23 22:37:31 -0400

    modificando build_files.sh 1.8

--- techlabs/wsgi.py
+++ techlabs/wsgi.py
@@ -10,8 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techlabs.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+
+# This is here for compatibility with Vercel
 app = application
